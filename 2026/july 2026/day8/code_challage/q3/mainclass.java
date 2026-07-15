f7b3aac1a6715dfcb93865ff7a326d551a466e1e
jeeva1978 <lakshmanvalli1978@gmail.com>
2026-07-15 22:31:57 +0530

    day 8 coding challage question 3

---
+++ 2026/july 2026/day8/code_challage/q3/mainclass.java
@@ -0,0 +1,10 @@
+package day8.code_challenge6.q3;
+final class constantvalues
+{
+	final double pi = 3.14;
+}
+//class cv extends constantvalues (this can not be use it because the constantvalues use the key word final that we cannot change anything from that )
+
+public class mainclass {
+
+}
