784dadde465031b5f31c34306a6aa0fc00f8db95
jeeva1978 <lakshmanvalli1978@gmail.com>
2026-07-23 22:36:05 +0530

    day 14 collection basic

---
+++ 2026/july 2026/day14/collection(Arraylist,hashmap)/q3.java
@@ -0,0 +1,14 @@
+package day14.Arraylist1;
+import java.util.HashSet;
+public class q3 {
+
+	public static void main(String[] args) {
+		HashSet <String> set = new HashSet<>();
+		// adding the elements
+		set.add("9677246382");
+		set.add("7401429206");
+		set.add("fjkjf");
+		System.out.println(set);
+	}
+
+}
